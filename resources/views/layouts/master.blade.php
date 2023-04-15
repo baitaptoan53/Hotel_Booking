4a455dd4b804218064baa11004a8906a95d7b088
Nguyễn Xuân Ngọc <84265122+baitaptoan53@users.noreply.github.com>
2023-04-15 19:44:39 +0700

    layout home

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -22,15 +22,11 @@
                <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css"
                               rel="stylesheet">
 
-               <!-- Libraries Stylesheet -->
                <link rel="stylesheet" href="{{'css/animate.min.css'}}">
                <link rel="stylesheet" href="{{'css/owl.carousel.min.css'}}">
+               <link rel="stylesheet" href="{{'css/tempusdominus-bootstrap-4.min.css'}}">
+               <link rel="stylesheet" href="{{'css/bootstrap.min.css'}}">
                <link rel="stylesheet" href="{{'css/style.css'}}">
-
-               <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css"
-                              rel="stylesheet"
-                              integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ"
-                              crossorigin="anonymous">
 </head>
 
 <body>
@@ -48,12 +44,20 @@
                               @yield('content')
                               @include('layouts.footer')
                </div>
-               <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
-                              integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
-                              crossorigin="anonymous"></script>
-                              <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-                              <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
-                              <script src="{{asset('js/main.js')}}"></script>
+               <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+               <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+               <script src="{{asset('js/main.js')}}"></script>
+               <script src="{{asset('js/counterup.min.js')}}"></script>
+               <script src="{{asset('js/waypoints.min.js')}}"></script>
+               <script src="{{asset('js/owl.carousel.min.js')}}"></script>
+               <script src="{{asset('js/wow.min.js')}}"></script>
+               <script src="{{asset('js/easing.min.js')}}"></script>
+               <script src="{{asset('js/moment-timezone.min.js')}}"></script>
+               <script src="{{asset('js/moment.min.js')}}"></script>
+               <script src="{{asset('js/tempusdominus-bootstrap-4.min.js')}}"></script>
+
+
+
 </body>
 
 </html>
